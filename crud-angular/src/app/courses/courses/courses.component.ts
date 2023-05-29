71a325eb622f9cc2a4a9b037ae014d509da90716
killua <killualolxx@gmail.com>
2023-05-28 21:57:23 -0300

    adicionado componentes e serviços da pagina de cursos

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit{
+
+  courses:Observable <Course[]>;
+  displayedColumns = ['name','category'];
+
+  constructor(private coursesService:CoursesService){
+    this.courses = this.coursesService.findAll();
+  }
+
+
+
+
+
+
+  ngOnInit(): void {
+
+  }
+
+
+
+}
